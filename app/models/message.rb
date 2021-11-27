1784825062952ecb1a71e0a17ae4dd455b9fa4a8
Yair Segal <Yair@jgive.com>
2021-11-27 23:20:32 +0200

    after update/destroy broadcast

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,6 @@
 
 class Message < ApplicationRecord
   after_create_commit { broadcast_prepend_to 'messages' }
+  after_update_commit { broadcast_replace_to 'messages' }
+  after_destroy_commit { broadcast_remove_to 'messages' }
 end
