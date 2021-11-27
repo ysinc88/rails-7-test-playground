1138e0aa31d6f7cddba724e58c85b9afad0568b2
Yair Segal <Yair@jgive.com>
2021-11-27 22:10:20 +0200

    dummy landing + dummy reset css

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :messages
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root to: 'pages#landing'
   # Almost every application defines a route for the root path ("/") at the top of this file.
   # root "articles#index"
 end
